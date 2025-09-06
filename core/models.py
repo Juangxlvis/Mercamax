f7c12299d94e467fa9a038d1b96be2cd29e63a27
Juangxlvis <galvis2044@gmail.com>
2025-09-05 22:35:49 -0500

    Add: notifications implemented

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import User
 
 class PerfilUsuario(models.Model):
@@ -13,6 +14,20 @@ class PerfilUsuario(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.get_rol_display()}"
+
+class Notificacion(models.Model):
+    class Tipo(models.TextChoices):
+        STOCK_BAJO = 'STOCK', 'Stock Bajo'
+        LOTE_VENCE = 'VENCE', 'Lote por Vencer'
+
+    usuario_destino = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    tipo = models.CharField(max_length=10, choices=Tipo.choices)
+    mensaje = models.CharField(max_length=255)
+    leida = models.BooleanField(default=False)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Alerta para {self.usuario_destino.username}: {self.mensaje}"
 
 # También es un buen lugar para poner la "señal" que crea el perfil automáticamente
 from django.db.models.signals import post_save
